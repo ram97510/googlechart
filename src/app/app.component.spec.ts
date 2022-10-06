8e98a5d8c938cc4ca54dbbd6aadf20d81b747a58
Ramkumar M <99495525+ram97510@users.noreply.github.com>
2022-10-06 12:35:00 +0530

    Add files via upload

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'googlechart'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('googlechart');
+    expect(app['title']).toEqual('googlechart');
   });
 
   it('should render title', () => {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as Highcharts from 'highcharts';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,208 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'googlechart';
+
+  //------------line chart----------
+  linechart = Highcharts;
+   lineOptions: Highcharts.Options = {   
+      chart: {
+         type: "spline"
+      },
+      title: {
+         text: "Monthly Average Temperature"
+      },
+      
+      xAxis:{
+         categories:["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+            "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+      },
+      yAxis: {          
+         title:{
+            text:"Temperature °C"
+         } 
+      },
+      tooltip: {
+         valueSuffix:" °C"
+      },
+      series: [
+         {
+            name: 'Chennai',
+            type: 'spline',
+            data: [23, 22, 28, 24, 25, 30]
+         },
+         {
+            name: 'Madurai',
+            type: 'spline',
+            data: [22, 27, 24, 26, 24, 25]
+         },
+         {
+            name: 'Ooty',
+            type: 'spline',
+            data: [18, 20, 16, 18, 17, 19]
+         },
+
+      ]
+   };
+
+  //------------------Bar chart---------------------
+
+  barchart = Highcharts;
+   barOptions :Highcharts.Options = {   
+      chart: {
+         type: 'bar'
+      },
+      title: {
+         text: 'School Strength'
+      },
+      
+      legend : {
+         layout: 'vertical',
+         align: 'center',
+         verticalAlign: 'top',
+         x: 250,
+         y: 100,
+         floating: true,
+         borderWidth: 1,
+        
+         },
+
+         xAxis:{
+            categories: ['9th', '10th', '11th', '12th'], 
+         },
+
+      yAxis : {
+        title: {
+            text: 'strength', align: 'high'
+         },
+         labels: {
+            overflow: 'justify'
+         }
+      },
+
+      tooltip : {
+         valueSuffix: ' thousand'
+      },
+      
+      plotOptions : {
+         bar: {
+            dataLabels: {
+               enabled: true
+            }
+         }
+      },
+      
+      series: [
+         {
+            name: 'Boys',
+            type: 'bar',
+            data: [207, 131, 335, 253, ]
+         }, 
+         {
+            name: 'Girls',
+            type: 'bar',
+            data: [133, 156, 347, 208,]
+         }, 
+       
+      ]
+   };
+
+  //------------Column Chart----------
+
+  columnchart = Highcharts;
+   columnOptions : Highcharts.Options = {   
+      chart: {
+         type: 'column'
+      },
+      title: {
+        text: "Strength"
+      },
+  
+      yAxis: {
+        max: 100
+      },
+      
+      tooltip: {
+        enabled: false
+      },
+      series: [
+        {
+          type: 'column',
+          data: [60],
+          name: "Boys",
+        },
+        {
+          type: 'column',
+          data: [40],
+          name: "Girls",
+        
+        }
+        
+      ]
+    };
+
+//-------------pie chart------------
+
+piechart = Highcharts;
+pieOptions : Highcharts.Options = {   
+   chart : {
+    type:'pie'
+
+   },
+   title : {
+      text: 'Marks in Subject wise'   
+   },
+   plotOptions: {
+    pie: {
+      allowPointSelect:true,
+      cursor: 'pointer',
+      dataLabels:{
+        enabled: true
+      },
+      showInLegend: true
+    }
+   },
+   
+   series : [{
+      type: 'pie',
+      name: 'Marks',
+      data: [
+         ['Tamil',   60],
+         ['English', 55],
+         ['Maths',   70],
+         ['Science', 83]
+      ]
+   }]
+};
+//--------------scatter chart-----------
+
+scatterchart = Highcharts;
+   scatterOptions : Highcharts.Options= {  
+             
+      title : {
+         text: 'Semester Mark'   
+      },     
+      
+      legend : {
+         layout: 'vertical',
+         align: 'center',
+         verticalAlign: 'top',
+         x: 250,
+         y: 100,
+         floating: true,
+         borderWidth: 1,
+        
+         },
+         
+      series : [{
+         type: 'scatter',
+         name: 'Sem I',
+         data: [ 63, 66, 70, 67, 73 ]
+      },
+      {
+      type: 'scatter',
+         name: 'Sem II',
+         data: [ 68, 69, 75, 65, 79 ]
+       }]
+   };
+
 }
